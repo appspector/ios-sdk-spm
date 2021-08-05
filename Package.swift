a0d1032625f66279f049584cff14f64481b53f27
AppSpector CI <heroes@techery.io>
2021-08-05 20:30:22 +0300

    SDK update: v 1.4.3 ()

--- Package.swift
+++ Package.swift
@@ -1,29 +1,29 @@
 // swift-tools-version:5.3
-    import PackageDescription
+import PackageDescription
 
-    let package = Package(
-        name: "AppSpector",
-        platforms: [
-            .iOS(.v13), .tvOS(.v13)
-        ],
-        products: [
-            .library(
-                name: "AppSpector",
-                targets: ["AppSpectorSDK"]),
-            .library(
-                name: "AppSpectorTVSDK",
-                targets: ["AppSpectorTVSDK"])
-        ],
-        targets: [
-            .binaryTarget(
-                name: "AppSpectorSDK",
-                url: "https://github.com/appspector/ios-sdk/releases/download/1.4.2/AppSpectorSDK.xcframework.zip",
-                checksum: "9593105cff7ffd91c79ba3a37b7e1a06a725023c7e85548e45836e1e73068a4b"
-            ),
-            .binaryTarget(
-                name: "AppSpectorTVSDK",
-                url: "https://github.com/appspector/ios-sdk/releases/download/1.4.2/AppSpectorTVSDK.xcframework.zip",
-                checksum: "a3f2008342c6fe2eae2acce8fe5ddc5a78167ef9561ec90eaa99a4da34610ed9"
-            )
-        ]
-    )
+let package = Package(
+    name: "AppSpector",
+    platforms: [
+        .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AppSpector",
+            targets: ["AppSpectorSDK"]),
+        .library(
+            name: "AppSpectorTVSDK",
+            targets: ["AppSpectorTVSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppSpectorSDK",
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.3/AppSpectorSDK.xcframework.zip",
+            checksum: "91f4d3a70ed9bc75720f381f7ab5f29e57e7337ec49fd8add359a47b89df1624"
+        ),
+        .binaryTarget(
+            name: "AppSpectorTVSDK",
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.3/AppSpectorTVSDK.xcframework.zip",
+            checksum: "b80e09d569cac61a310172e17fead341d7b53912d8ace8c5295a52dede9b1233"
+        )
+    ]
+)
