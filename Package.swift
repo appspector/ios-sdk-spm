5b8ae7bf38d315a483856a11539089c4810e433b
AppSpector CI <heroes@techery.io>
2021-11-23 20:03:26 +0200

    SDK update: v 1.4.5

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .binaryTarget(
             name: "AppSpectorSDKE2E",
             url: "https://github.com/appspector/ios-sdk/releases/download/1.4.5/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "19eeb4c08654599ef9a9c14340d8c9caf3c2fcc25eeb4f25e17152897b030e4c"
+            checksum: "78064690dcd0a7127ac8e013ec29ca0b4ec61620b31921d181b505059c3520b8"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
