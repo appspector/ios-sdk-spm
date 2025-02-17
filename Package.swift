483379ff53fae4911dd0c49c18ddde57333471bd
deszip <raf.rafiki@gmail.com>
2025-02-17 23:43:24 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSpectorSDK",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.13/AppSpectorSDK.xcframework.zip",
-            checksum: "f1d832844615ad9f4987b121406cea4ea4cd846c3b20fb8161f78447d3887820"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.5/AppSpectorSDK.xcframework.zip",
+            checksum: "404ebe16d688e3d2591a0e2deed2768236f0ae1764f45a8bf50fec432029dcbf"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
-            url: "https://github.com/appspector/ios-sdk/releases/download/1.4.13/AppSpectorTVSDK.xcframework.zip",
-            checksum: "d084ef890748699683436ddccb7b72ba774a91949892227a1c63600fdc33c55a"
+            url: "https://github.com/appspector/ios-sdk/releases/download/1.5/AppSpectorTVSDK.xcframework.zip",
+            checksum: "edcc39fe5e36d4ed75687a42c29bdd2c1f482af63d963c84e143c62fe3c28366"
         )
     ]
 )
